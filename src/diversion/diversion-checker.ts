5c1370a1baf942907b8f7d4b69973107fc385486
Woody Beverley <woodybeverley@gmail.com>
2023-11-27 10:47:53 -0800

    Removed commented out console.logs

--- src/diversion/diversion-checker.ts
+++ src/diversion/diversion-checker.ts
@@ -11,21 +11,17 @@ function fibonacci(n: number): number {
     let n_minus_2 = 1;
 
     if (n <= 0) {
-        //console.log('0');
         return 0;
     }
     if (n === 1) {
-        //console.log('1');
         return 1;
     }
 
     for (let i = 0; i < n; i++) {
         const temp = n_minus_1 + n_minus_2;
-        //console.log(temp);
         n_minus_2 = n_minus_1;
         n_minus_1 = temp;
     }
 
     return n_minus_1;
-
 }
